aed0ecfae8461c4545a99214e731a34a70b2f93b
AbdulMateen <Leo@DESKTOP-R3UEMAK>
2020-07-22 00:17:06 +0500

    done

--- LabTest3_CM_068/Program.cs
+++ LabTest3_CM_068/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,49 @@ namespace LabTest3_CM_068
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Creating file......");
+            string fileName = @"task1.txt";
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+                Console.WriteLine("Creating random numbers list....");
+                using (FileStream fs = File.Create(fileName))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        string line = "";
+                        for (int j = 0; j < 5; j++)
+                            line += generateRandomNumber() + "\t";
+                        Byte[] title = new UTF8Encoding(true).GetBytes(line);
+                        fs.Write(title, 0, title.Length);
+                        byte[] newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+                        fs.Write(newline, 0, newline.Length);
+                    }
+                }
+            }
+            catch (IOException Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            catch (NullReferenceException Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+            Console.WriteLine("Random Number saved successfully in file");
+            Console.ReadKey();
+        }
+        static Random rnd = new Random();
+        static int generateRandomNumber()
+        {
+            return rnd.Next(0, 1001);
         }
     }
 }
